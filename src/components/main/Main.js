3f1322dbb01d407a1c33f0af80bd5a44fedd57a7
Alena Yarmak <alena.yarmak@protonmail.com>
2023-02-06 20:11:32 +0100

    change some styles

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -55,32 +55,32 @@ class Main extends Component {
             cards.push(<div className="main__card">
             <div className="main__card__title">{key}</div>
             <div className="main__card__value">{value}</div>
-            <div className="main__card__text"></div></div>);
+            </div>);
         }
 
         if (airQualityLevel === 1) {
             description = <div className="main__response">
-            <p className="main__response-txt">The level of air quality</p> <p className="main__response-txt">{airQualityLevel}</p> <p className="main__response-txt">It's mean the level of air quality is good. Composition of air:</p>
+            <p className="main__response-txt">The level of air quality</p> <p className="main__response-txt">{airQualityLevel}.</p> <p className="main__response-txt">It's mean the level of air quality is good. Composition of air:</p>
         </div>
         } 
         if (airQualityLevel === 2) {
             description = <div className="main__response">
-            <p className="main__response-txt">The level of air quality</p> <p className="main__response-txt">{airQualityLevel}</p> <p className="main__response-txt">It's mean the level of air quality is fair. Composition of air:</p>
+            <p className="main__response-txt">The level of air quality</p> <p className="main__response-txt">{airQualityLevel}.</p> <p className="main__response-txt">It's mean the level of air quality is fair. Composition of air:</p>
         </div>
         }
         if (airQualityLevel === 3) {
             description = <div className="main__response">
-            <p className="main__response-txt">The level of air quality</p> <p className="main__response-txt">{airQualityLevel}</p> <p className="main__response-txt">It's mean the level of air quality is moderate. Composition of air:</p>
+            <p className="main__response-txt">The level of air quality</p> <p className="main__response-txt">{airQualityLevel}.</p> <p className="main__response-txt">It's mean the level of air quality is moderate. Composition of air:</p>
         </div>
         }
         if (airQualityLevel === 4) {
             description = <div className="main__response">
-            <p className="main__response-txt">The level of air quality</p> <p className="main__response-txt">{airQualityLevel}</p> <p className="main__response-txt">It's mean the level of air quality is poor. Composition of air:</p>
+            <p className="main__response-txt">The level of air quality</p> <p className="main__response-txt">{airQualityLevel}.</p> <p className="main__response-txt">It's mean the level of air quality is poor. Composition of air:</p>
         </div>
         }
         if (airQualityLevel === 5) {
             description = <div className="main__response">
-            <p className="main__response-txt">The level of air quality</p> <p className="main__response-txt">{airQualityLevel}</p> <p className="main__response-txt">It's mean the level of air quality is very poor. Composition of air:</p>
+            <p className="main__response-txt">The level of air quality</p> <p className="main__response-txt">{airQualityLevel}.</p> <p className="main__response-txt">It's mean the level of air quality is very poor. Composition of air:</p>
         </div>
         }
         
